784ca1c8510d96ab559a585d9f1e455abcd6a35d
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2021-03-28 12:07:01 +0300

    byveee
    terrible fixes

--- src/fag.js
+++ src/fag.js
@@ -9,7 +9,8 @@
 </article>`
 */
 
-const tagCss = 'max-width: 1200px;width: 85%;margin:30px auto;'
+
+const tagCss = 'max-width: 1200px;margin:30px auto;'
 const descriptionTag = 'p'
 
 class Fag {
@@ -23,7 +24,9 @@ class Fag {
 		if(this.path) document.write(`<link rel='stylesheet' href='${this.path}files/FAG_style.css' type='text/css'>`);
 		else document.write(`<link rel='stylesheet' href='FAG_style.css' type='text/css'>`);
 
-		if(options.data) this.data = options.data // for json data
+		if(options.data) {
+			this.data = options.data  // for json data
+		} 
 		else console.log('not data')
 
 		if(options.tag){
@@ -31,6 +34,8 @@ class Fag {
 			this.tag.style.cssText = tagCss
 			this.renderGallery()
 		}
+
+		//this.tag.style.height = (1200/3)*Math.ceil(this.data.length/3) + "px"
 	}
 
 
@@ -38,13 +43,19 @@ class Fag {
 
 		let f = this.data.map((el,index)=>{
 
+
 			let img, descTag, aside, item
 
 			el.src?img=el.src:this.path?img=`${this.path}files/file.svg`:img="files/file.svg"
-			el.text?descTag=`<${descriptionTag}>${el.text}</${descriptionTag}>`:""
+			el.text?descTag=`<${descriptionTag}>${el.text}</${descriptionTag}>`:" "
 			aside = `<aside style="background-image:url(${img})">${descTag}</aside>`
 
 			el.link?item=`<article><a href="${el.link}">${aside}</a></article>`:item=`<article>${aside}</article>`
+
+			if(!((index)%3!=0)) item = '<div class="FAG_row">' + item
+			if(!((index+1)%3!=0) || this.data.length-1 == index) item = item + '</div>'
+
+			console.log(item)
 
 			if(this.headlines){
 				if(el.visible && this.headlines.indexOf(el.title) != -1){
@@ -67,7 +78,6 @@ class Fag {
 		}
 		else this.tag.innerHTML = f.join('')
 
-		
 		let objs = this.tag.querySelectorAll("article")
 		objs.forEach((obj, index)=>{
 			if((index+1)%3!=0) obj.className = "FAG_item"
@@ -75,9 +85,6 @@ class Fag {
 			obj.querySelector("a")?obj.querySelector("a").className = 'FAG_link':''
 			obj.querySelector("aside").className = 'FAG_aside'
 			obj.querySelector(descriptionTag).className = 'FAG_p'
-		})
+		}) 
 	}
 }
-
-
-
