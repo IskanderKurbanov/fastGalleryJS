8d6294c01330e5d265013c8227499562c720d933
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2021-04-27 17:43:09 +0300

    v0.0.38

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # fastGalleryjs(~dataPreviewjs)
-- beta v0.0.35
+- simple test library for yourself
 
-simple test library for yourself
 
 ![alt text](https://raw.githubusercontent.com/IskanderKurbanov/fastGalleryJS/main/img/preview.png)
 
@@ -17,27 +16,27 @@ Quickstart:
 
 <script type="text/javascript">
 	data = [{
-		title: "first",
-		text: "byveeeeeeeee.",
+		title: "work",
+		text: "first work.",
 		mobileText:' shit sketch #7',
 		img: "https://64.media.tumblr.com/153d6a0b837b5c6acf4e0126cb0a09c1/d05fc402daf519f7-a9/s1280x1920/c3f0b55775e8b70b5f49d99f56a551ff287a68ca.png",
 	},
 	{
-		title: "first",
-		text: "byveeeeeeeee",
+		title: "work",
+		text: "second work",
 		mobileText:'shit sketch #8',
 		img: "https://64.media.tumblr.com/8a0794dff8fdd9fe0583670b30d57707/61cca6543a97d912-08/s540x810/89bbb7971ee2cc4f1e51cb1bd2a3602643f28f3f.png",
 	},{
-		title: "first",
+		title: "sketch",
 		text: "fail sketch #5",
 		mobileText: "fail sketch #5",
 		img: "https://64.media.tumblr.com/de99fc88b35565130f913f29e3207beb/9353f3a9455a1520-4b/s1280x1920/f5d739847fac41d5f51caa6fb8f71f0035cf8f62.png",
 	},{
-		title: "first",
+		title: "work",
 		text: "frogy",
 		img: "https://64.media.tumblr.com/b9a5742fbfa0abff576af12effc33d75/14b0813d76682e6b-4b/s1280x1920/2ac6bcb2ab2dc12c93c612cf37391805093dd8bb.png",
 	},{
-		title: "title",
+		title: "sketch",
 		text: "shit sketch #4",
 		mobileText:'shit sketch #4',
 		img: "https://64.media.tumblr.com/1f70f4fb234c8d083cc7415080d70274/3edddcca7b00b547-82/s1280x1920/e62aa45cc30617d5d9ba7f10cf4cddc39f650cf8.png",
@@ -46,7 +45,6 @@ Quickstart:
 	new Fag({
 		tag: "#fg",
 		data: data,
-		//headlines: ['first', 'title'],
 	},)
 </script>
 ```
@@ -61,19 +59,4 @@ default data:
 		link: '',
 	}
 
-if your api data not as default data, you can change they:
-```javascript
-
-	new Fag({
-		tag: "#fg",
-		data: data,
-		defaultKey: {
-			// default key: your json key
-			title: 'name',
-			text: 'description',
-			img: 'url',
-		}
-	},)
-
-```
 
--- src/js/fag.js
+++ src/js/fag.js
@@ -17,116 +17,58 @@ const descriptionTag = 'p'
 class Fag {
 
 	constructor(options) {
-		//options:{tag, headlines, path, data}
-
 		this.width = document.body.clientWidth
-
 		this.headlines = options.headlines // for gallery's headlines
-
+		this.tools = options.tools
 		if(options.data) {
 			this.data = options.data  // for json data
 		}
 		else console.log('not data')
-
-		if(options.defaultKey){
-			this.data.forEach(el=>{	
-				if(options.defaultKey.title) {
-					Object.defineProperty(el, 'title',  Object.getOwnPropertyDescriptor(el, options.defaultKey.title));
-					delete el[options.defaultKey.title];
-				}
-				if(options.defaultKey.text) {
-					Object.defineProperty(el, 'text',  Object.getOwnPropertyDescriptor(el, options.defaultKey.text));
-					delete el[options.defaultKey.text];
-				}
-				if(options.defaultKey.mobileText) {
-					Object.defineProperty(el, 'mobileText',  Object.getOwnPropertyDescriptor(el, options.defaultKey.mobileText));
-					delete el[options.defaultKey.mobileText];
-				}
-				if(options.defaultKey.img) {
-					Object.defineProperty(el, 'img',  Object.getOwnPropertyDescriptor(el, options.defaultKey.img));
-					delete el[options.defaultKey.img];
-				}
-				if(options.defaultKey.link) {
-					Object.defineProperty(el, 'link',  Object.getOwnPropertyDescriptor(el, options.defaultKey.link));
-					delete el[options.defaultKey.link];
-				}		
-			})
-		}
-
 		if(options.tag){
 			this.tag = document.querySelector(options.tag) // html tag where gallery is located
-			this.tag.style.cssText = tagCss
+			if(this.tools) this.renderTools(this.tools)
+			options.tagStyle==undefined||options.tagStyle==''?this.tag.style.cssText = tagCss : this.tag.style.cssText = options.tagStyle
 			this.renderGallery(this.width)
 		}
-
 		document.querySelector('.FAG_tools').querySelectorAll('aside').forEach(el=>{
 			el.addEventListener("click", ()=>this.postRender(el.getAttribute('data')))
-			el.addEventListener("touchend", ()=>this.postRender(el.getAttribute('data')))
+			//el.addEventListener("touchend", ()=>this.postRender(el.getAttribute('data')))
 		})
 	}
 
 
 	postRender(format){
-		let objs, objsRows
-		(format=='_list')? objsRows = this.tag.querySelectorAll(".FAG_row"): objsRows = this.tag.querySelectorAll(".FAG_row_list")
-		objs = this.tag.querySelectorAll("article")
-
-		if(format!='_list'){
-			objs.forEach((obj, index)=>{
-				((index+1)%3!=0)?obj.className = "FAG_item":obj.className = "FAG_item_th"
-				
-				obj.querySelector("a")?obj.querySelector("a").className = 'FAG_link':''
-				obj.querySelector("aside").className = 'FAG_aside'
-				obj.querySelector(descriptionTag).className = 'FAG_p'
-			})
-		} else {
-			objs.forEach((obj, index)=>{
-				((index+1)%3!=0)?obj.className = "FAG_item"+format:obj.className = "FAG_item_th"+format
-				
-				obj.querySelector("a")?obj.querySelector("a").className = 'FAG_link':''
-				obj.querySelector("aside").className = 'FAG_aside'+format
-				obj.querySelector(descriptionTag).className = 'FAG_p'
+		let objsRows
+		format=='_list'? objsRows = this.tag.querySelectorAll(".FAG_row"): objsRows = this.tag.querySelectorAll(".FAG_row_list")
+		this.tag.querySelectorAll("article").forEach((obj, index)=>{
+			((index+1)%3!=0)?obj.className = "FAG_item"+format:obj.className = "FAG_item_th"+format
+			obj.querySelector("a")?obj.querySelector("a").className = 'FAG_link':''
+			obj.querySelector("aside").className = 'FAG_aside'+format
+			obj.querySelector(descriptionTag).className = 'FAG_p'
+		})
+		objsRows.forEach((obj)=> obj.className="FAG_row"+format )
+		if(this.tools){ 
+			document.querySelector('.FAG_tools').querySelectorAll('aside').forEach(el=>{
+				(el.getAttribute('data') == format)? el.className='FAG_active':el.className=''
 			})
 		}
-		objsRows.forEach((obj)=>{
-			obj.className="FAG_row"+format
-		})
-		document.querySelector('.FAG_tools').querySelectorAll('aside').forEach(el=>{
-			(el.getAttribute('data') == format)? el.className='FAG_active':el.className=''
-		})
 	}
 
 
-	renderGallery(width) {
-		let format = '_list'
-
-		let f = this.data.map((el,index)=>{
-
-
-			let img, descTag, mobDescTag, aside, item
-
-			if(el.img) img=el.img
-
-			el.text ? descTag=`<${descriptionTag}>${el.text}</${descriptionTag}>` : descTag=`<${descriptionTag}> ${index+1} </${descriptionTag}>`
-			el.mobileText ? mobDescTag=`<span class="FAG_mobileP">${el.mobileText}</span>`: mobDescTag=`<span class="FAG_mobileP"></span>`
-
-			aside = `<aside style="background-image:url(${img})">${descTag}${mobDescTag}</aside>`
-
-			el.link?item = `<article><a href="${el.link}">${aside}</a></article>` : item=`<article>${aside}</article>`
-
-			if(!((index)%3!=0)) item = '<div class="FAG_row">' + item
-			if(!((index+1)%3!=0) || this.data.length-1 == index) item = item + '</div>'
-
-			if(this.headlines){
-				if(this.headlines.indexOf(el.title) != -1){
-					return item
-				}
-			}
-			else {return item}
-		})
+	renderTools(tools) {
+		let view, statistic
+		tools.view?view = '<aside data="">row</aside><aside data="_list">list</aside>':view = ''
+		tools.statistic?statistic = 'statistic: cooming soon':statistic = ''
+		const tools_block = document.createElement('div')
+		this.tag.append(tools_block)
+		tools_block.innerHTML = `<div class="FAG_tools">
+									${statistic}
+									${view}
+								</div>`
+	}
 
 
-		
+	renderHeadlines(f, format) {
 		if(this.headlines) {
 			let m = this.headlines.map((el, index)=>{return `<p>${el}</p>`})
 			this.tag.innerHTML = '<nav class="FAG_nav">'+m.join('')+'</nav>' + f.join('')
@@ -134,8 +76,34 @@ class Fag {
 			let navigation = this.tag.querySelector("nav")
 			navigation.querySelectorAll("p").forEach(el=>el.className = 'FAG_nav_p')
 		}
-		else this.tag.innerHTML = `<div class="FAG_tools"><aside data="">row</aside><aside data="${format}">list</aside></div>` + f.join('')
+		return f.join('')
+	}
 
+
+	renderGallery(width) {
+		let format = '_list'
+		const data_block = document.createElement('div')
+		this.tag.append(data_block)
+
+		let f = this.data.map((el,index)=>{
+			let img, descTag, mobDescTag, aside, item
+			if(el.img) img=el.img
+			el.text ? descTag=`<${descriptionTag}>${el.text}</${descriptionTag}>` : descTag=`<${descriptionTag}> ${index+1} </${descriptionTag}>`
+			el.mobileText ? mobDescTag=`<span class="FAG_mobileP">${el.mobileText}</span>`: mobDescTag=`<span class="FAG_mobileP"></span>`
+			aside = `<aside style="background-image:url(${img})">${descTag}${mobDescTag}</aside>`
+			el.link?item = `<article><a href="${el.link}">${aside}</a></article>` : item=`<article>${aside}</article>`
+			if(!((index)%3!=0)) item = '<div class="FAG_row">' + item
+			if(!((index+1)%3!=0) || this.data.length-1 == index) item = item + '</div>'
+			if(this.headlines){
+				if(this.headlines.indexOf(el.title) != -1){
+					return item
+				}
+			}
+			return item
+		})
+
+		let done_data = this.renderHeadlines(f, format)
+		data_block.innerHTML = done_data
 		width<=1000?this.postRender(format):this.postRender('')
 	}
 }
